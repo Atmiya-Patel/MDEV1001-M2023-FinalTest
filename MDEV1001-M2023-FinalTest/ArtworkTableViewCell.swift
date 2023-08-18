925f6a8930cb96bfdea11e158e863706877e03b9
Atmiya-Patel <atmiya.2011@gmail.com>
2023-08-18 15:57:06 -0400

    added code for CRUD & UI

--- MDEV1001-M2023-FinalTest/ArtworkTableViewCell.swift
+++ MDEV1001-M2023-FinalTest/ArtworkTableViewCell.swift
@@ -2,21 +2,12 @@ import UIKit
 
 class ArtworkTableViewCell: UITableViewCell {
     
-    @IBOutlet weak var titleLabel: UILabel!
     
-    @IBOutlet weak var artistsLabel: UILabel!
+    @IBOutlet weak var titleLabel: UITextField!
     
-    @IBOutlet weak var mediumLabel: UILabel!
+    @IBOutlet weak var artistLabel: UITextField!
+
     
-    @IBOutlet weak var subjectsLabel: UILabel!
-    
-    @IBOutlet weak var yearCreatedLabel: UILabel!
-    
-    @IBOutlet weak var descriptionLabel: UILabel!
-    
-    @IBOutlet weak var dimensionsLabel: UILabel!
-    
-    @IBOutlet weak var posterImageView: UIImageView!
-    
+   @IBOutlet weak var posterImageView : UIImageView!
     
 }
